0c602cbc94d6fa4d15510637f0b895fc449e00ef
nandhastr <nandhastr2@gmail.com>
2025-07-24 15:51:02 +0700

    fixbug server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-import dotenv from "dotenv";
 
 import app from "./api/index.js";
 
-dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port =  3000;
 app.listen(port, () => {
     console.log(`Server berjalan di port: ${port}`);
 });
